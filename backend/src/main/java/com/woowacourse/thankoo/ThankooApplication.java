7c9766529b27b4edda56b1023011eacee52fa41f
skullKim <kyk990328@gmail.com>
2022-06-30 15:40:57 +0900

    chore: 프로젝트 세팅
    필요한 의존성 추가, property 파일 설정

---
+++ backend/src/main/java/com/woowacourse/thankoo/ThankooApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.thankoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThankooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThankooApplication.class, args);
+    }
+
+}
