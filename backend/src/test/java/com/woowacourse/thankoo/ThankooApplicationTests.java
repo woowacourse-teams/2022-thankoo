3f80f5871f34d20487aeb65b7aecb46f179633a1
skullKim <kyk990328@gmail.com>
2022-06-30 15:40:57 +0900

    chore: 프로젝트 세팅
    필요한 의존성 추가, property 파일 설정

---
+++ backend/src/test/java/com/woowacourse/thankoo/ThankooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.thankoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThankooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
