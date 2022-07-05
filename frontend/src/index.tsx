64fac78cebf653e4d18bf248629441fe67bfd4e5
kwannee <yleh1126@naver.com>
2022-07-05 10:39:46 +0900

    feat: 전역 style resset
    Co-authored-by: KangYunHo1221 <ansghkdbsgh@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+import reset from './styles/GlobalStyles';
+import { Global } from '@emotion/react';
+
 const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <App />
   </React.StrictMode>
 );
