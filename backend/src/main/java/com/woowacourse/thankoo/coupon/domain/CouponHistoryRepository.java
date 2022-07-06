5e66770e3ec4bb9343d2b3a238bd301eb57fc639
skullKim <kyk990328@gmail.com>
2022-07-06 13:27:01 +0900

    refactor: 사용하지 않는 메서드 삭제

--- backend/src/main/java/com/woowacourse/thankoo/coupon/domain/CouponHistoryRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/coupon/domain/CouponHistoryRepository.java
@@ -1,9 +1,6 @@
 package com.woowacourse.thankoo.coupon.domain;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponHistoryRepository extends JpaRepository<CouponHistory, Long> {
-
-    List<CouponHistory> findByReceiverId(Long id);
 }
