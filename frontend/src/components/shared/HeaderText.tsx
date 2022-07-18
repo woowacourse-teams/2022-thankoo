20542675031d3b13a85f0dad7ba0fc6c39fddd04
Yunho <ansghkdbsgh@gmail.com>
2022-07-18 19:40:47 +0900

    chore: 불필요 충돌파일 삭제

--- frontend/src/components/shared/HeaderText.tsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-const HeaderText = styled.p`
-  font-size: 22px;
-  margin-left: calc(1vw + 6px);
-`;
-
-export default HeaderText;
--- frontend/src/components/shared/PageLayout.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-const PageLayout = styled.section`
-  display: flex;
-  flex-direction: column;
-  padding: 5px;
-`;
-
-export default PageLayout;
