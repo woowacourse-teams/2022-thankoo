aec28686583897fdb3cc0131eebedc7e1586b59b
Yunho <ansghkdbsgh@gmail.com>
2022-07-19 15:41:03 +0900

    feat: reservation mocking 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,4 +1,9 @@
+import { createReservationHandler } from './createReservationHandler';
 import { mainPageHandler } from './mainPageHandler';
 import { selectReceiverPageHandlers } from './selectReceiverPageHandlers';
 
-export const handlers = [...mainPageHandler, ...selectReceiverPageHandlers];
+export const handlers = [
+  ...mainPageHandler,
+  ...selectReceiverPageHandlers,
+  ...createReservationHandler,
+];
