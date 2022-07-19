58d4159a0e2f229080ae01b7e5ccf70a58a9a0e2
Yunho <ansghkdbsgh@gmail.com>
2022-07-19 16:38:55 +0900

    feat: date가 차 있을때만 약속신청 활성화 기능 추가

--- frontend/src/hooks/CreateReservation/useCreateReservation.ts
+++ frontend/src/hooks/CreateReservation/useCreateReservation.ts
@@ -8,7 +8,7 @@ import { authAtom } from '../../recoil/atom';
 const useCreateReservation = () => {
   const [date, setDate] = useState('');
   const { accessToken } = useRecoilValue(authAtom);
-  const isFilled = true;
+  const isFilled = date;
 
   const { mutate } = useMutation((Date: string) =>
     axios({
@@ -27,13 +27,7 @@ const useCreateReservation = () => {
   };
 
   const sendReservation = () => {
-    if (isValidReservationDate()) {
-      mutate(date);
-    }
-  };
-
-  const isValidReservationDate = () => {
-    return date.length >= 16;
+    mutate(date);
   };
 
   return { isFilled, setReservationDate, sendReservation };
