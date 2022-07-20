6d41d9259d221fe17ca7e0db751ebfc0e07d3e2f
kwannee <yleh1126@naver.com>
2022-07-20 15:10:07 +0900

    refactor: 사용 안하는 기능 제거

--- frontend/src/hooks/Reservations/useReservations.ts
+++ frontend/src/hooks/Reservations/useReservations.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 import { API_PATH } from '../../constants/api';
-import { Coupon } from '../../types';
 
 const orderByList = ['date', 'name'];
 const orderByObject = { date: '날짜 순', name: '이름 순' };
@@ -18,35 +17,15 @@ const useResrvations = () => {
     },
     { refetchOnWindowFocus: false }
   );
-  const [checkedCoupons, setCheckedCoupons] = useState<Coupon[]>([]);
-  const checkCoupon = (coupon: Coupon) => {
-    setCheckedCoupons(prev => [...prev, coupon]);
-  };
-  const uncheckCoupon = (coupon: Coupon) => {
-    setCheckedCoupons(prev =>
-      prev.filter(checkedCoupon => checkedCoupon.couponHistoryId !== coupon.couponHistoryId)
-    );
-  };
-  const toggleCoupon = (coupon: Coupon) => {
-    if (isCheckedCoupon(coupon)) {
-      uncheckCoupon(coupon);
-      return;
-    }
-    checkCoupon(coupon);
-  };
-  const isCheckedCoupon = (coupon: Coupon) =>
-    checkedCoupons?.some(checkCoupon => checkCoupon.couponHistoryId === coupon.couponHistoryId);
+
+  // reservations?.sort();
 
   return {
     orderBy,
     setOrderBy,
     reservations,
-    checkCoupon,
-    uncheckCoupon,
-    toggleCoupon,
     orderByList,
     orderByObject,
-    isCheckedCoupon,
   };
 };
 
