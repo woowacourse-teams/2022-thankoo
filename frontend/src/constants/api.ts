619da39ac4d61ab681c2e4d2ccd420d95efe0d42
Yunho <ansghkdbsgh@gmail.com>
2022-07-20 21:22:40 +0900

    refactor: url 수정

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,13 +1,11 @@
-export const BASE_URL = 'http://localhost:3000';
-export const AUTH_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://54.180.102.68:8080';
 
 const API_PREFIX = `${BASE_URL}/api`;
-const AUTH_PREFIX = `${AUTH_URL}/api`;
 
 export const API_PATH = {
   RECEIVED_COUPONS: `${API_PREFIX}/coupons/received`,
   MEMBERS: `${API_PREFIX}/members`,
   RESERVATIONS: `${API_PREFIX}/reservations`,
   SEND_COUPON: `${API_PREFIX}/coupons/send`,
-  SIGN_IN: userCode => `${AUTH_PREFIX}/sign-in?code=${userCode}`,
+  SIGN_IN: userCode => `${API_PREFIX}/sign-in?code=${userCode}`,
 };
