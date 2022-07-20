d4d5f5611781addf83a2d678207d6da154655f85
sure-why-not <pkse1213@gmail.com>
2022-07-20 10:59:41 +0900

    style: 불필요한 import 제거

--- backend/src/test/java/com/woowacourse/thankoo/reservation/application/ReservationServiceTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/reservation/application/ReservationServiceTest.java
@@ -15,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import com.woowacourse.thankoo.common.support.DataClearExtension;
 import com.woowacourse.thankoo.coupon.domain.Coupon;
 import com.woowacourse.thankoo.coupon.domain.CouponContent;
 import com.woowacourse.thankoo.coupon.domain.CouponRepository;
@@ -30,7 +29,6 @@ import java.time.LocalDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
