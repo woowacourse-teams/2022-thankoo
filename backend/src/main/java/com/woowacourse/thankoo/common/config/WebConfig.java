3e2d98ecf24a95ce964f4b6c9b8dd95d1c0bb9eb
jayjaehunchoi <wogns0108@naver.com>
2022-07-21 16:07:15 +0900

    refactor: cors origin 추가

--- backend/src/main/java/com/woowacourse/thankoo/common/config/WebConfig.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedOrigins("http://thankoo.co.kr", "http://localhost:3000")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders(HttpHeaders.LOCATION);
     }
