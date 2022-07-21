e9d4510ddbbf95b0ef4ac4c61ea2970866685b36
kwannee <yleh1126@naver.com>
2022-07-21 15:21:16 +0900

    feat: 예약 요청 시 시간도 포함해서 보내도록 수정

--- frontend/src/hooks/CreateReservation/useCreateReservation.ts
+++ frontend/src/hooks/CreateReservation/useCreateReservation.ts
@@ -3,32 +3,48 @@ import { useState } from 'react';
 import { useMutation } from 'react-query';
 import { API_PATH } from '../../constants/api';
 
+const yesterday = new Date().toISOString().split('T')[0];
+
 const useCreateReservation = () => {
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(yesterday);
+  const [time, setTime] = useState('10:00:00');
+
   const accessToken = localStorage.getItem('token');
   const isFilled = date;
 
-  const { mutate } = useMutation((Date: string) =>
+  const { mutate } = useMutation((startAt: string) =>
     axios({
       method: 'post',
       url: `${API_PATH.RESERVATIONS}`,
       headers: { Authorization: `Bearer ${accessToken}` },
       data: {
         couponId: '1',
-        startAt: Date,
+        startAt,
       },
     })
   );
 
   const setReservationDate = e => {
-    setDate(e.target.value + ` 00:00:00`);
+    setDate(e.target.value);
   };
 
   const sendReservation = () => {
-    mutate(date);
+    mutate(`${date} ${time}`);
   };
 
-  return { isFilled, setReservationDate, sendReservation };
+  const setReservationTime = e => {
+    setTime(e.target.value);
+  };
+
+  return {
+    isFilled,
+    setReservationDate,
+    sendReservation,
+    date,
+    yesterday,
+    time,
+    setReservationTime,
+  };
 };
 
 export default useCreateReservation;
