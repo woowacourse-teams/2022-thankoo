adece6ef1bdab311394082806a300582e252bf1e
sure-why-not <pkse1213@gmail.com>
2022-07-24 19:14:37 +0900

    refactor: @Transactional 추가

--- backend/src/main/java/com/woowacourse/thankoo/reservation/application/ReservationQueryService.java
+++ backend/src/main/java/com/woowacourse/thankoo/reservation/application/ReservationQueryService.java
@@ -8,8 +8,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReservationQueryService {
 
