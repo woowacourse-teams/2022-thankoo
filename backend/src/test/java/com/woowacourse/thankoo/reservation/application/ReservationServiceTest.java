12ac5ff650ed48d99e99837e5cd87dfb9a93c9c1
sure-why-not <pkse1213@gmail.com>
2022-07-25 15:41:08 +0900

    test: 예약 생성 테스트 수정

--- backend/src/test/java/com/woowacourse/thankoo/reservation/application/ReservationServiceTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/reservation/application/ReservationServiceTest.java
@@ -2,6 +2,9 @@ package com.woowacourse.thankoo.reservation.application;
 
 import static com.woowacourse.thankoo.common.fixtures.CouponFixture.MESSAGE;
 import static com.woowacourse.thankoo.common.fixtures.CouponFixture.TITLE;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_EMAIL;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_NAME;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_SOCIAL_ID;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.IMAGE_URL;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.LALA_EMAIL;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.LALA_NAME;
@@ -22,12 +25,12 @@ import com.woowacourse.thankoo.coupon.domain.CouponStatus;
 import com.woowacourse.thankoo.coupon.exception.InvalidCouponException;
 import com.woowacourse.thankoo.member.domain.Member;
 import com.woowacourse.thankoo.member.domain.MemberRepository;
-import com.woowacourse.thankoo.member.exception.InvalidMemberException;
 import com.woowacourse.thankoo.reservation.application.dto.ReservationRequest;
 import com.woowacourse.thankoo.reservation.application.dto.ReservationStatusRequest;
 import com.woowacourse.thankoo.reservation.domain.Reservation;
 import com.woowacourse.thankoo.reservation.domain.ReservationRepository;
 import com.woowacourse.thankoo.reservation.domain.ReservationStatus;
+import com.woowacourse.thankoo.reservation.exception.InvalidReservationException;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -69,30 +72,31 @@ class ReservationServiceTest {
         );
     }
 
-    @DisplayName("예약시 쿠폰이 존재하는지 검증한다.")
+    @DisplayName("예약시 쿠폰이 존재하지 않을 경우 에외가 발생한다.")
     @Test
     void isExistedCoupon() {
-        Long memberId = 1L;
+        Member member = memberRepository.save(new Member(LALA_NAME, LALA_EMAIL, LALA_SOCIAL_ID, IMAGE_URL));
 
-        assertThatThrownBy(() -> reservationService.save(memberId,
+        assertThatThrownBy(() -> reservationService.save(member.getId(),
                 new ReservationRequest(3L, LocalDateTime.now().plusDays(1L))))
                 .isInstanceOf(InvalidCouponException.class)
                 .hasMessage("존재하지 않는 쿠폰입니다.");
     }
 
-    @DisplayName("예약을 보내는 회원과 쿠폰을 받은 회원이 같은지 검증한다.")
+    @DisplayName("예약을 보내는 회원이 쿠폰을 받은 회원이 아닐 경우 예외가 발생한다.")
     @Test
     void isSameReceiver() {
-        Long receiverId = 2L;
-        Long invalidReceiverId = 3L;
+        Member sender = memberRepository.save(new Member(LALA_NAME, LALA_EMAIL, LALA_SOCIAL_ID, IMAGE_URL));
+        Member receiver = memberRepository.save(new Member(SKRR_NAME, SKRR_EMAIL, SKRR_SOCIAL_ID, IMAGE_URL));
+        Member other = memberRepository.save(new Member(HUNI_NAME, HUNI_EMAIL, HUNI_SOCIAL_ID, IMAGE_URL));
 
         Coupon coupon = couponRepository.save(
-                new Coupon(1L, receiverId, new CouponContent(COFFEE, TITLE, MESSAGE), NOT_USED));
+                new Coupon(sender.getId(), receiver.getId(), new CouponContent(COFFEE, TITLE, MESSAGE), NOT_USED));
 
-        assertThatThrownBy(() -> reservationService.save(invalidReceiverId,
+        assertThatThrownBy(() -> reservationService.save(other.getId(),
                 new ReservationRequest(coupon.getId(), LocalDateTime.now().plusDays(1L))))
-                .isInstanceOf(InvalidMemberException.class)
-                .hasMessage("존재하지 않는 회원입니다.");
+                .isInstanceOf(InvalidReservationException.class)
+                .hasMessage("예약을 요청할 수 없는 회원입니다.");
     }
 
     @DisplayName("예약을 승인한다.")
