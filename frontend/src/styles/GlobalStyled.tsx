53c3eb821682a73221067e46f5109623fdda1376
kwannee <yleh1126@naver.com>
2022-07-27 15:28:45 +0900

    feat: font 적용

--- frontend/src/styles/GlobalStyled.tsx
+++ frontend/src/styles/GlobalStyled.tsx
@@ -1,6 +1,14 @@
 import { css } from '@emotion/react';
 
 const global = css`
+  * {
+    font-family: 'Noto Sans KR', sans-serif !important;
+  }
+  @font-face {
+    font-family: 'Noto Sans KR', sans-serif;
+    src: url('/assets/fonts/NotoSansKR-Regular.otf') format('truetype');
+  }
+
   button {
     cursor: pointer;
   }
