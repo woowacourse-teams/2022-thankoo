e20e5e8939b0f2a606449640c1b8cc86c9393d1a
kwannee <yleh1126@naver.com>
2022-07-28 12:30:40 +0900

    feat: 빈 페이지 예약에도 추가
    Co-authored-by KangYunHo1221 <ansghkdbsgh@gmail.com>

--- frontend/src/mocks/reservationsHandlers.js
+++ frontend/src/mocks/reservationsHandlers.js
@@ -2,6 +2,47 @@ import { rest } from 'msw';
 import { API_PATH } from '../constants/api';
 import { dummyCoupons } from './dummyData';
 
+const dummyReceivedReservations = [
+  {
+    reservationId: 1,
+    memberName: '호호',
+    couponType: 'coffee',
+    meetingTime: '2022-07-23T12:40:41.617785',
+  },
+  {
+    reservationId: 2,
+    memberName: '숟갈',
+    couponType: 'coffee',
+    meetingTime: '2022-07-23T12:40:41.617808',
+  },
+  {
+    reservationId: 3,
+    memberName: '라라',
+    couponType: 'meal',
+    meetingTime: '2022-07-23T12:40:41.617813',
+  },
+];
+const dummySentReservations = [
+  {
+    reservationId: 4,
+    memberName: '후니의 식사쿠폰',
+    couponType: 'meal',
+    meetingTime: '2022-07-22T12:40:00.681091',
+  },
+  {
+    reservationId: 5,
+    memberName: '호호의 카누쿠폰',
+    couponType: 'coffee',
+    meetingTime: '2022-07-23T12:40:41.681112',
+  },
+  {
+    reservationId: 6,
+    memberName: '호호의 카누쿠폰',
+    couponType: 'coffee',
+    meetingTime: '2022-07-23T12:40:41.681116',
+  },
+];
+
 export const reservationsHanlders = [
   rest.get(`${API_PATH.RESERVATIONS}`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(dummyCoupons));
@@ -12,54 +53,10 @@ export const reservationsHanlders = [
     return res(ctx.status(200));
   }),
   rest.get(`${API_PATH.RESERVATIONS_RECEIVED}`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          reservationId: 1,
-          memberName: '호호',
-          couponType: 'coffee',
-          meetingTime: '2022-07-23T12:40:41.617785',
-        },
-        {
-          reservationId: 2,
-          memberName: '숟갈',
-          couponType: 'coffee',
-          meetingTime: '2022-07-23T12:40:41.617808',
-        },
-        {
-          reservationId: 3,
-          memberName: '라라',
-          couponType: 'meal',
-          meetingTime: '2022-07-23T12:40:41.617813',
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json(dummyReceivedReservations));
   }),
   rest.get(`${API_PATH.RESERVATIONS_SENT}`, (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json([
-        {
-          reservationId: 4,
-          memberName: '후니의 식사쿠폰',
-          couponType: 'meal',
-          meetingTime: '2022-07-22T12:40:00.681091',
-        },
-        {
-          reservationId: 5,
-          memberName: '호호의 카누쿠폰',
-          couponType: 'coffee',
-          meetingTime: '2022-07-23T12:40:41.681112',
-        },
-        {
-          reservationId: 6,
-          memberName: '호호의 카누쿠폰',
-          couponType: 'coffee',
-          meetingTime: '2022-07-23T12:40:41.681116',
-        },
-      ])
-    );
+    return res(ctx.status(200), ctx.json(dummySentReservations));
   }),
   rest.put(`${API_PATH.RESERVATIONS}/:id`, (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({}));
