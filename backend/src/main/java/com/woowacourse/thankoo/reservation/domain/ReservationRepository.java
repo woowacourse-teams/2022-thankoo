714e1f82884aa5581cd1ebf2d84fb2bfed633248
skullKim <kyk990328@gmail.com>
2022-07-28 11:03:42 +0900

    style: 코드 컨벤션 수정
    Co-authored-by: jayjaehunchoi <wogns0108@naver.com>

--- backend/src/main/java/com/woowacourse/thankoo/reservation/domain/ReservationRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/reservation/domain/ReservationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    @EntityGraph(attributePaths = {"coupon"}, type = EntityGraphType.LOAD)
+    @EntityGraph(attributePaths = "coupon", type = EntityGraphType.LOAD)
     Optional<Reservation> findWithCouponById(Long reservationId);
 }
