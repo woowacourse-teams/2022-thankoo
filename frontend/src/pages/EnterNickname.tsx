fc6a1ff3233e0dba751ed72301c9af16b1856cd1
kwannee <yleh1126@naver.com>
2022-07-28 10:43:31 +0900

    feat: Nickname 입력 UI 구현

--- frontend/src/pages/EnterNickname.tsx
+++ frontend/src/pages/EnterNickname.tsx
@@ -1,8 +1,11 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { requestInstance } from '../apis/axios';
 import Header from '../components/@shared/Header';
 import HeaderText from '../components/@shared/HeaderText';
+import Input from '../components/@shared/Input';
 import PageLayout from '../components/@shared/PageLayout';
 import { ROUTE_PATH } from '../constants/routes';
 import { saveAuth } from '../utils/auth';
@@ -11,34 +14,88 @@ const EnterNickname = () => {
   //useQueryClinet로 email, nickname, token
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const { email, memberId } = queryClient.getQueryData('token') as any;
+  const [nickname, setNickname] = useState('');
+  // const { email, memberId } = queryClient.getQueryData('token') as any;
 
-  const onclick = () => {
-    //받아온걸 바탕으로 api 요청을 보냄
-    const data = useQuery(
-      '닉네임입력',
-      () => {
-        requestInstance({
-          method: 'post',
-        });
-      },
-      {
-        onSuccess: data => {
-          saveAuth(data.accesstoken);
-          navigate(ROUTE_PATH.EXACT_MAIN); //redirect to exactMain
-        },
-      }
-    );
-  };
+  // const onclick = () => {
+  //   //받아온걸 바탕으로 api 요청을 보냄
+  //   const data = useQuery(
+  //     '닉네임입력',
+  //     () => {
+  //       const { data } = requestInstance({
+  //         method: 'post',
+  //       });
+
+  //       return data;
+  //     },
+  //     {
+  //       onSuccess: ({ accessToken }) => {
+  //         saveAuth(accessToken);
+  //         navigate(ROUTE_PATH.EXACT_MAIN); //redirect to exactMain
+  //       },
+  //     }
+  //   );
+  // };
 
   return (
-    <PageLayout>
+    <S.PageLayout>
       <Header>
-        <HeaderText>닉네임을 입력해주세요</HeaderText>
-        <button onClick={onclick}>닉네임 제출</button>
+        <HeaderText>회원가입을 완료해주세요!</HeaderText>
       </Header>
-    </PageLayout>
+      <S.Body>
+        <S.Form>
+          <S.FlexColumn>
+            <S.Label htmlFor='email'>이메일</S.Label>
+            <S.Email>hoho@gmail.com</S.Email>
+          </S.FlexColumn>
+          <S.FlexColumn>
+            <S.Label htmlFor='nickname'>닉네임</S.Label>
+            <Input
+              id='nickname'
+              value={nickname}
+              setValue={setNickname}
+              placeholder='닉네임을 입력해주세요'
+            />
+          </S.FlexColumn>
+          <S.Button>회원가입 완료</S.Button>
+        </S.Form>
+      </S.Body>
+    </S.PageLayout>
   );
 };
 
 export default EnterNickname;
+
+const S = {
+  PageLayout: styled(PageLayout)`
+    padding: 10rem 0;
+    gap: 50px;
+  `,
+  Body: styled.section`
+    padding: 1rem;
+    color: white;
+  `,
+  Form: styled.form`
+    display: flex;
+    flex-flow: column;
+    gap: 15px;
+  `,
+  FlexColumn: styled.div`
+    display: flex;
+    flex-flow: column;
+    gap: 7px;
+  `,
+  Email: styled.span`
+    color: #8e8e8e;
+    font-size: 18px;
+  `,
+  Label: styled.label``,
+  Button: styled.button`
+    color: white;
+    border-radius: 10px;
+    border: none;
+    padding: 15px;
+    font-size: 17px;
+    background-color: ${({ theme }) => theme.primary};
+  `,
+};
