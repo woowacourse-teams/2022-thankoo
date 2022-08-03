2110c945f141e560f519110cc41b5d9a692912c0
Jaehun Choi (후니) <wogns0108@naver.com>
2022-08-03 15:51:19 +0900

    refactor: 반환 값에 소문자, 날짜 포맷을 수정한다. (#204)
    * docs: 문서에 각 status 반영
    
    * refactor: 응답 포맷 변경 (소문자, 타임 포맷)

--- backend/src/main/java/com/woowacourse/thankoo/meeting/presentation/dto/MeetingResponse.java
+++ backend/src/main/java/com/woowacourse/thankoo/meeting/presentation/dto/MeetingResponse.java
@@ -1,7 +1,9 @@
 package com.woowacourse.thankoo.meeting.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.woowacourse.thankoo.meeting.domain.MeetingCoupon;
 import java.time.LocalDateTime;
+import java.util.Locale;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,8 @@ import lombok.NoArgsConstructor;
 public class MeetingResponse {
 
     private Long id;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime meetingTime;
     private String couponType;
     private String memberName;
@@ -21,7 +25,7 @@ public class MeetingResponse {
                            final String memberName) {
         this.id = id;
         this.meetingTime = meetingTime;
-        this.couponType = couponType;
+        this.couponType = couponType.toLowerCase(Locale.ROOT);
         this.memberName = memberName;
     }
 
