d014183fc35d039722147c66eef7e5b2e0ba2f84
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-08-03 19:59:59 +0900

    feat: 하루 지난 미팅을 완료 처리한다. (#201)
    * feat: Meeting 완료
    
    * feat: Meeting 완료 api
    
    * docs: indexing
    
    * test: 트랜잭션 반영안되는 테스트 변경
    
    * refactor: 메서드 명 변경
    
    * refactor: final 제거
    
    * feat: 새벽2시에 하루 지난 미팅 완료 처리
    
    * refactor: 필드명 수정
    
    * fix: 기존 코드로 복원
    
    * refactor: 스케줄 관련 메서드 네이밍 수정
    
    * docs: 스케줄 관련 주석 설명 추가
    
    * refactor: 네이밍 수정
    
    * refactor: 변수명 수정
    
    * fix: 미팅 스케줄링 삭제시 쿠폰 상태가 변경되지 않는 문제 수정
    
    * style: 공백 추가
    
    * test: 이전 테스트 코드로 복원
    
    * refactor: 메서드 네이밍 수정
    
    Co-authored-by: jayjaehunchoi <wogns0108@naver.com>

---
+++ backend/src/main/java/com/woowacourse/thankoo/common/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.woowacourse.thankoo.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
