2eed8155770a0d7ee52cf363faacfe428cb6cf0b
yhh1056 <yhh1056@naver.com>
2022-08-03 13:20:29 +0900

    docs: 스케줄 관련 주석 설명 추가

--- backend/src/main/java/com/woowacourse/thankoo/meeting/domain/MeetingScheduleRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/meeting/domain/MeetingScheduleRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface MeetingScheduleRepository extends MeetingRepository {
 
+    /**
+     * 해당 메서드는 스케줄 관련 작업에서 사용한다.
+     */
     @Modifying(clearAutomatically = true)
     @Query(value = "UPDATE Meeting m SET m.status = ? WHERE m.date = ? AND m.status = ?", nativeQuery = true)
     void updateMeetingStatus(String updatedStatus, LocalDate date, String originStatus);
