4aee932629f46f076fb07908bfb322b97600fbd6
jayjaehunchoi <wogns0108@naver.com>
2022-08-03 09:29:16 +0900

    refactor: meeting 조회시 fetch join 하도록 변경

--- backend/src/main/java/com/woowacourse/thankoo/meeting/domain/MeetingRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/meeting/domain/MeetingRepository.java
@@ -2,8 +2,11 @@ package com.woowacourse.thankoo.meeting.domain;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
-    Optional<Meeting> findTopByCouponIdAndMeetingStatus(Long couponId, MeetingStatus status);
+    @Query("SELECT DISTINCT m FROM Meeting m LEFT JOIN FETCH m.meetingMembers.meetingMembers mm WHERE m.coupon.id = :couponId AND m.meetingStatus = :status")
+    Optional<Meeting> findTopByCouponIdAndMeetingStatus(@Param("couponId") Long couponId, @Param("status") MeetingStatus status);
 }
