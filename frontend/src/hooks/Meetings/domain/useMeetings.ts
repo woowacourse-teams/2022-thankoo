a2b2ce79c028edf72ba693f7aaa383fb809ab108
sure-why-not <pkse1213@gmail.com>
2022-08-05 12:54:21 +0900

    fix: meeting list error

--- frontend/src/hooks/Meetings/domain/useMeetings.ts
+++ frontend/src/hooks/Meetings/domain/useMeetings.ts
@@ -2,6 +2,15 @@ import { useEffect, useState } from 'react';
 import { Meeting } from '../../../types';
 import { useGetMeetings } from '../queries/useGetMeetings';
 
+const dayDifferenceFromToday = (meetingDay) =>{
+  const today = new Date();
+
+  const dayForCal = `${meetingDay.getFullYear()}` + `${String(meetingDay.getMonth()).padStart(2,'0')}` + `${String(meetingDay.getDay()).padStart(2,'0')}`;
+  const todayForCal = `${today.getFullYear()}` + `${String(today.getMonth()).padStart(2,'0')}` + `${String(today.getDay()).padStart(2,'0')}`;
+  
+  return Number(dayForCal) - Number(todayForCal);
+}
+
 const useMeetings = () => {
   const { data: meetings, isLoading, isError, isSuccess } = useGetMeetings();
   const [diffWithNearestDate, setDiffWithNearestDate] = useState<number>(0);
@@ -9,26 +18,22 @@ const useMeetings = () => {
 
   meetings?.sort(
     (m1, m2) =>
-      Number(new Date(m1.time?.meetingTime as string)) -
-      Number(new Date(m2.time?.meetingTime as string))
-  );
+      Number(new Date(m1.time?.meetingTime.replaceAll('-','/'))) -
+      Number(new Date(m2.time?.meetingTime.replaceAll('-','/'))));
 
   useEffect(() => {
     if (meetings) {
-    
+      const meetingDay = new Date(meetings[0]?.time?.meetingTime.replaceAll('-','/').split(' ')[0])
+   
       setDiffWithNearestDate(
-        Math.floor(
-          (Number(new Date(meetings[0]?.time?.meetingTime.replaceAll('-','/'))) - Number(new Date())) /
-            (1000 * 60 * 60 * 24)
-        )
+        dayDifferenceFromToday(meetingDay)
       );
-      setMeeting(meetings);
     }
   }, [isSuccess]);
 
   const isTodayMeetingExist = diffWithNearestDate === 0;
 
-  return { meeting, meetings, isLoading, isError, isTodayMeetingExist, diffWithNearestDate };
+  return { meetings, isLoading, isError, isTodayMeetingExist, diffWithNearestDate };
 };
 
 export default useMeetings;
