67e8a628cbc379d16f8d7c97618a5526b4dd7812
Yunho <ansghkdbsgh@gmail.com>
2022-08-12 13:29:19 +0900

    fix: dev와 prod의 clientID 분리

--- frontend/src/constants/googleAuth.ts
+++ frontend/src/constants/googleAuth.ts
@@ -1,6 +1,8 @@
 const GOOGLE_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 const CLIENT_ID =
-  'client_id=119823899527-vi7230j5vt9lf8s4aiapcao4pf4n9d98.apps.googleusercontent.com';
+  process.env.NODE_ENV === 'development'
+    ? 'client_id=119823899527-vi7230j5vt9lf8s4aiapcao4pf4n9d98.apps.googleusercontent.com'
+    : 'client_id=135992368964-20ad4ul4e3mmia6iok3r9dpg6bshp4uq.apps.googleusercontent.com';
 const REDIRECT_URL =
   process.env.NODE_ENV === 'development'
     ? 'redirect_uri=http://localhost:3000/sign-in'
