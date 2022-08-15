c947da2ad9f14588332da7d0a0461bd5ab4f8f73
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-08-15 14:54:45 +0900

    refactor: 알람 관련 기능을 리팩터링한다. (#270)
    * refactor: coupons 일급컬렉션 추가
    
    * refactor: meetings, coupons 일급컬렉션 추가
    
    * refactor: 스케줄 관련 코드 meetingService 로 이동
    
    * refactor: userId 조회 테스트 코드 추가
    
    * refactor: userToken 이라는 네이밍으로 userId 의미 통일
    
    * refactor: 알람 관련 예외클래스 변경
    
    * refactor: 알람 관련 테스트 코드 추가
    
    * refactor: 변수명 수정, 불필요한 어노테이션 삭제, 에러 메시지 수정
    
    * test: 테스트 중복코드 제거
    
    * fix: 테스트 환경에서 알람 관련 스레드가 지워지지 않는 문제 해결
    
    * style: 공백 제거

--- backend/src/main/java/com/woowacourse/thankoo/common/schedule/MeetingScheduleTask.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/schedule/MeetingScheduleTask.java
@@ -1,24 +1,10 @@
 package com.woowacourse.thankoo.common.schedule;
 
-import static com.woowacourse.thankoo.meeting.domain.MeetingStatus.ON_PROGRESS;
-
-import com.woowacourse.thankoo.alarm.AlarmMessage;
-import com.woowacourse.thankoo.alarm.support.AlarmMessageRequest;
-import com.woowacourse.thankoo.alarm.support.Alarm;
-import com.woowacourse.thankoo.alarm.support.AlarmManager;
-import com.woowacourse.thankoo.coupon.domain.CouponRepository;
-import com.woowacourse.thankoo.coupon.domain.CouponStatus;
-import com.woowacourse.thankoo.meeting.domain.Meeting;
-import com.woowacourse.thankoo.meeting.domain.MeetingRepository;
-import com.woowacourse.thankoo.meeting.domain.MeetingStatus;
+import com.woowacourse.thankoo.meeting.application.MeetingService;
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -26,49 +12,15 @@ public class MeetingScheduleTask {
 
     public static final Long DAY = 1L;
 
-    private final MeetingRepository meetingRepository;
-    private final CouponRepository couponRepository;
+    private final MeetingService meetingService;
 
     @Scheduled(cron = "0 0 2 * * *")
-    @Transactional
     public void executeCompleteMeeting() {
-        List<Meeting> meetings = meetingRepository.findAllByMeetingStatusAndTimeUnit_Date(
-                ON_PROGRESS, LocalDate.now().minusDays(DAY));
-
-        List<Long> meetingIds = getMeetingIds(meetings);
-        List<Long> couponIds = getCouponIds(meetings);
-
-        meetingRepository.updateMeetingStatus(MeetingStatus.FINISHED, meetingIds);
-        couponRepository.updateCouponStatus(CouponStatus.USED, couponIds);
+        meetingService.complete(LocalDate.now().minusDays(DAY));
     }
 
-    private List<Long> getMeetingIds(final List<Meeting> meetings) {
-        return meetings.stream()
-                .map(Meeting::getId)
-                .collect(Collectors.toList());
-    }
-
-    private List<Long> getCouponIds(final List<Meeting> meetings) {
-        return meetings.stream()
-                .map(meeting -> meeting.getCoupon().getId())
-                .collect(Collectors.toList());
-    }
-
-    @Alarm
     @Scheduled(cron = "0 0 9 * * *")
     public void executeMeetingMessage() {
-        List<Meeting> meetings = meetingRepository.findAllByTimeUnit_Date(LocalDate.now());
-        List<String> emails = getEmails(meetings);
-        AlarmManager.setResources(new AlarmMessageRequest(emails, AlarmMessage.MEETING));
-        AlarmManager.clear();
-    }
-
-    private List<String> getEmails(final List<Meeting> meetings) {
-        return meetings.stream()
-                .map(meeting -> meeting.getMeetingMembers().getMeetingMembers())
-                .flatMap(Collection::stream)
-                .map(meetingMember -> meetingMember.getMember().getEmail().getValue())
-                .distinct()
-                .collect(Collectors.toList());
+        meetingService.sendMessageTodayMeetingMembers(LocalDate.now());
     }
 }
---
+++ backend/src/main/java/com/woowacourse/thankoo/member/domain/Members.java
@@ -0,0 +1,23 @@
+package com.woowacourse.thankoo.member.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Members {
+
+    private final List<Member> values;
+
+    public Members(final List<Member> values) {
+        this.values = List.copyOf(values);
+    }
+
+    public List<String> getEmails() {
+        return values.stream()
+                .map(member -> member.getEmail().getValue())
+                .collect(Collectors.toList());
+    }
+
+    public boolean isEmpty() {
+        return values.isEmpty();
+    }
+}
