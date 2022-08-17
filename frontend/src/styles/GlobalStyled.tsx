2abe4d38269f708c17dd9519f7b2abadc8114830
Yunho <ansghkdbsgh@gmail.com>
2022-08-18 02:31:42 +0900

    refactor: SignInPage, PageLayout rem변환작업

--- frontend/src/styles/GlobalStyled.tsx
+++ frontend/src/styles/GlobalStyled.tsx
@@ -5,6 +5,11 @@ const global = css`
     font-family: 'Noto Sans KR', sans-serif !important;
     -webkit-tap-highlight-color: transparent;
   }
+  html {
+    //Todo 반응형으로 fontSize로 조정하기 default = 3
+    //1. Se 기준
+    font-size: 10px;
+  }
 
   button {
     cursor: pointer;
