f029961b15385b56511cdb5ba799192e7665a318
jayjaehunchoi <wogns0108@naver.com>
2022-08-18 09:41:08 +0900

    refactore: coupon 단건 조회 순서 변경

--- backend/src/test/java/com/woowacourse/thankoo/coupon/domain/CouponStatusTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/coupon/domain/CouponStatusTest.java
@@ -42,11 +42,19 @@ class CouponStatusTest {
         assertThat(couponStatus.isReserved()).isTrue();
     }
 
-    @DisplayName("쿠폰이 사용된 상태인지 확인한다.")
+    @DisplayName("쿠폰이 만남 확인한다.")
     @Test
     void isUsed() {
         CouponStatus couponStatus = CouponStatus.USED;
 
         assertThat(couponStatus.isUsed()).isTrue();
+    }
+
+    @DisplayName("쿠폰 상태를 조회한다.")
+    @ParameterizedTest(name = "{index} {displayName} status={0}, isRight={1}")
+    @CsvSource(value = {"NOT_USED:false", "RESERVING:true", "RESERVED:true", "USED:true",
+            "EXPIRED:false"}, delimiter = ':')
+    void isInReserveOrUsed(CouponStatus status, Boolean isRight) {
+        assertThat(status.isInReserveOrUsed()).isEqualTo(isRight);
     }
 }
