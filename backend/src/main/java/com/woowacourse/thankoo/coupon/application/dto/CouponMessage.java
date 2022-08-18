d4a1bb4cd5eaa5561c5535fc0b374f165f6b2537
yhh1056 <yhh1056@naver.com>
2022-08-18 15:49:31 +0900

    fix: 식사쿠폰 제목 수정

--- backend/src/main/java/com/woowacourse/thankoo/coupon/application/dto/CouponMessage.java
+++ backend/src/main/java/com/woowacourse/thankoo/coupon/application/dto/CouponMessage.java
@@ -15,7 +15,8 @@ public class CouponMessage {
     private static final String SENDER = "보내는 이 : {0}";
     private static final String TITLE = "제목 : {0}";
     private static final String TYPE = "쿠폰 종류 : {0}";
-    private static final String PRETEXT = "\uD83D\uDC8C 커피 쿠폰이 도착했어요.";
+    private static final String COFFEE_PRETEXT = "\uD83D\uDC8C 커피 쿠폰이 도착했어요.";
+    private static final String MEAL_PRETEXT = "\uD83D\uDC8C 식사 쿠폰이 도착했어요.";
     private static final Map<String, String> type = Map.of(
             "coffee", "커피☕",
             "meal", "식사\uD83C\uDF54");
@@ -23,10 +24,17 @@ public class CouponMessage {
     public static Message of(final Name name, final List<String> emails, final CouponContent couponContent) {
         return new Message.Builder()
                 .email(emails)
-                .title(PRETEXT)
+                .title(getTitle(couponContent.getCouponType().getValue()))
                 .content(MessageFormat.format(SENDER, name.getValue()))
                 .content(MessageFormat.format(TITLE, couponContent.getTitle()))
                 .content(MessageFormat.format(TYPE, type.get(couponContent.getCouponType().getValue())))
                 .build();
+    }
+
+    public static String getTitle(final String couponType) {
+        if (couponType.equalsIgnoreCase("COFFEE")) {
+            return COFFEE_PRETEXT;
+        }
+        return MEAL_PRETEXT;
     }
 }
