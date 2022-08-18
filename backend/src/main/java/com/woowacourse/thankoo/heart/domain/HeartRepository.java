c683cc26f3c41fa0e06ab06e34cb28e50373ae89
jayjaehunchoi <wogns0108@naver.com>
2022-08-18 17:26:12 +0900

    refactor: heart 조회 기능 변경

--- backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
@@ -1,9 +1,14 @@
 package com.woowacourse.thankoo.heart.domain;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Optional<Heart> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
+
+    List<Heart> findBySenderIdAndLast(Long senderId, boolean last);
+
+    List<Heart> findByReceiverIdAndLast(Long receiverId, boolean last);
 }
--- backend/src/test/java/com/woowacourse/thankoo/heart/application/HeartServiceTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/heart/application/HeartServiceTest.java
@@ -1,21 +1,31 @@
 package com.woowacourse.thankoo.heart.application;
 
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HOHO_EMAIL;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HOHO_NAME;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HOHO_SOCIAL_ID;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_EMAIL;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_NAME;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.HUNI_SOCIAL_ID;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.IMAGE_URL;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.LALA_EMAIL;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.LALA_NAME;
+import static com.woowacourse.thankoo.common.fixtures.MemberFixture.LALA_SOCIAL_ID;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.SKRR_EMAIL;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.SKRR_NAME;
 import static com.woowacourse.thankoo.common.fixtures.MemberFixture.SKRR_SOCIAL_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.thankoo.common.annotations.ApplicationTest;
 import com.woowacourse.thankoo.heart.application.dto.HeartRequest;
 import com.woowacourse.thankoo.heart.domain.HeartRepository;
 import com.woowacourse.thankoo.heart.exception.InvalidHeartException;
+import com.woowacourse.thankoo.heart.presentation.dto.HeartResponse;
+import com.woowacourse.thankoo.heart.presentation.dto.HeartResponses;
 import com.woowacourse.thankoo.member.domain.Member;
 import com.woowacourse.thankoo.member.domain.MemberRepository;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -72,5 +82,32 @@ class HeartServiceTest {
                     .isInstanceOf(InvalidHeartException.class)
                     .hasMessage("마음을 보낼 수 없습니다.");
         }
+    }
+
+    @DisplayName("연달아 보낼 수 없는 보낸 마음과 응답할 수 있는 받은 마음을 모두 조회한다.")
+    @Test
+    void getEachHeartsLast() {
+        Member huni = memberRepository.save(new Member(HUNI_NAME, HUNI_EMAIL, HUNI_SOCIAL_ID, IMAGE_URL));
+        Member skrr = memberRepository.save(new Member(SKRR_NAME, SKRR_EMAIL, SKRR_SOCIAL_ID, IMAGE_URL));
+        Member lala = memberRepository.save(new Member(LALA_NAME, LALA_EMAIL, LALA_SOCIAL_ID, IMAGE_URL));
+        Member hoho = memberRepository.save(new Member(HOHO_NAME, HOHO_EMAIL, HOHO_SOCIAL_ID, IMAGE_URL));
+
+        heartService.send(skrr.getId(), new HeartRequest(huni.getId()));
+        heartService.send(lala.getId(), new HeartRequest(huni.getId()));
+        heartService.send(hoho.getId(), new HeartRequest(huni.getId()));
+
+        heartService.send(huni.getId(), new HeartRequest(lala.getId()));
+
+        HeartResponses heartResponses = heartService.getEachHeartsLast(huni.getId());
+        List<HeartResponse> sentHearts = heartResponses.getSent();
+        List<HeartResponse> receivedHearts = heartResponses.getReceived();
+        assertAll(
+                () -> assertThat(sentHearts).hasSize(1),
+                () -> assertThat(sentHearts).extracting("receiverId")
+                        .containsExactly(lala.getId()),
+                () -> assertThat(receivedHearts).hasSize(2),
+                () -> assertThat(receivedHearts).extracting("senderId")
+                        .containsExactly(skrr.getId(), hoho.getId())
+        );
     }
 }
