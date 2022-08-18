aa944b0ee7245871be0d58e7b402e36e3be4b9c7
yhh1056 <58363663+yhh1056@users.noreply.github.com>
2022-08-18 21:20:51 +0900

    refactor: 슬랙 메시지를 한글로 전송한다. (#326)
    * feat: 슬랙 알림 메시지 한글 전송
    
    * test: 슬랙 메시지 테스트
    
    * style: 공백 추가
    
    * fix: 식사쿠폰 제목 수정
    
    * fix: 예약 취소시 닉네임으로 전송되도록 수정
    
    * refactor: 사용하지 않는 픽스처 제거
    
    * refactor: 슬랙 메시지에 땡쿠 링크 메시지 수정
    
    * refactor: 메시지 빌더 패턴 수정
    
    * test: 실패하는 테스트 케이스
    
    * feat: 도메인별 링크로 이동하도록 수정

--- backend/src/main/java/com/woowacourse/thankoo/meeting/application/dto/MeetingMessage.java
+++ backend/src/main/java/com/woowacourse/thankoo/meeting/application/dto/MeetingMessage.java
@@ -1,7 +1,6 @@
 package com.woowacourse.thankoo.meeting.application.dto;
 
 import com.woowacourse.thankoo.alarm.support.Message;
-import com.woowacourse.thankoo.alarm.support.Message.Builder;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,10 +8,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MeetingMessage {
 
-    public static final String PRETEXT = "\uD83E\uDD70 오늘은 미팅이 있는 날이에요!!";
+    private static final String PRETEXT = "\uD83E\uDD70 오늘은 미팅이 있는 날이에요!!";
+    private static final String TITLE_LINK = "https://thankoo.co.kr/meetings";
 
     public static Message of(final List<String> emails) {
-        return new Builder()
+        return Message.builder()
+                .titleLink(TITLE_LINK)
                 .email(emails)
                 .title(PRETEXT)
                 .build();
