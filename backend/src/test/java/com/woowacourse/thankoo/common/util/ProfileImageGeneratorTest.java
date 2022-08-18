0dde7a9de3852e7c8245bbd1ce198f2ca49cde43
Yunki Kim <37966705+skullkim@users.noreply.github.com>
2022-08-18 19:45:59 +0900

    fix: jar파일 실행 시 파일 못읽어오는 문제 해결 (#335)
    * fix: jar파일 실행 시 파일 못읽어오는 문제 해결
    
    * chore: 필요 없는 테스트 삭제
    
    * refactor: 테스트 패키지 이동

--- backend/src/test/java/com/woowacourse/thankoo/common/util/ProfileImageGeneratorTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/common/util/ProfileImageGeneratorTest.java
@@ -1,16 +1,22 @@
-package com.woowacourse.thankoo.authentication.util;
+package com.woowacourse.thankoo.common.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.thankoo.common.exception.BadRequestException;
 import com.woowacourse.thankoo.common.util.ProfileImageGenerator;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 @DisplayName("RandomProfileImageGenerator 는 ")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
