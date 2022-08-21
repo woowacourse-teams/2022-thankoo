2310fea860f9fc44c3bebffa782ac7bbac46160a
yhh1056 <yhh1056@naver.com>
2022-08-21 18:40:11 +0900

    refactor: 마스킹 변수명 수정

--- backend/src/test/java/com/woowacourse/thankoo/common/logging/MaskingUtilTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/common/logging/MaskingUtilTest.java
@@ -11,8 +11,8 @@ class MaskingUtilTest {
     @Test
     @DisplayName("지정된 범위 이후의 문자를 마스킹한다.")
     void mask() {
-        String maskedToken = MaskingUtil.mask("eyJhb.eyJzdWIiOiIxMjM0NTY3ODk.SflKxwRJSMe", 3);
+        String maskedWord = MaskingUtil.mask("eyJhb.eyJzdWIiOiIxMjM0NTY3ODk.SflKxwRJSMe", 3);
 
-        assertThat(maskedToken).isEqualTo("eyJ*******");
+        assertThat(maskedWord).isEqualTo("eyJ*******");
     }
 }
