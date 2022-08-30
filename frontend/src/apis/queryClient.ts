5b099e905fe662c8c85664b338c6e3d1b5717e69
Yunho <ansghkdbsgh@gmail.com>
2022-08-30 15:18:31 +0900

    merge

--- frontend/src/apis/queryClient.ts
+++ frontend/src/apis/queryClient.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { AxiosError } from 'axios';
 import { QueryClient } from 'react-query';
 import { ROUTE_PATH } from '../constants/routes';
@@ -55,3 +56,59 @@ const defaultQueryClientOptions = {
 export const queryClient = new QueryClient({
   defaultOptions: defaultQueryClientOptions,
 });
+=======
+import { AxiosError } from 'axios';
+import { QueryClient } from 'react-query';
+import { ROUTE_PATH } from '../constants/routes';
+import { clearAuth } from '../utils/auth';
+
+const INVALID_AUTH_ERROR_CODE = 2001;
+const INVALID_AUTH_STATUS = 401;
+
+type AuthErrorResponse = {
+  data: { errorCode: number };
+};
+
+const queryErrorHandler = error => {
+  authErrorHandler(error);
+};
+const mutateErrorHandler = error => {
+  authErrorHandler(error);
+};
+const retryHandler = (failureCount, error) => {
+  if (
+    error.response.status === INVALID_AUTH_STATUS ||
+    error.response.data.errorCode === INVALID_AUTH_ERROR_CODE
+  ) {
+    return false;
+  }
+
+  return true;
+};
+
+const authErrorHandler = (error: AxiosError) => {
+  const {
+    data: { errorCode },
+  } = error?.response as AuthErrorResponse;
+
+  if (errorCode === INVALID_AUTH_ERROR_CODE) {
+    clearAuth();
+    window.location.replace(`${ROUTE_PATH.SIGN_IN}`);
+  }
+};
+
+const defaultQueryClientOptions = {
+  queries: {
+    onError: queryErrorHandler,
+    retry: retryHandler,
+  },
+  mutations: {
+    onError: mutateErrorHandler,
+    retry: retryHandler,
+  },
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: defaultQueryClientOptions,
+});
+>>>>>>> 277b11d (merge)
--- frontend/src/hooks/Reservations/queries/reservations.ts
+++ frontend/src/hooks/Reservations/queries/reservations.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useMutation, useQueryClient } from 'react-query';
 import { client } from '../../../apis/axios';
 import { API_PATH } from '../../../constants/api';
@@ -29,3 +30,37 @@ export const usePutReservationStatus = (
     }
   );
 };
+=======
+import { useMutation, useQueryClient } from 'react-query';
+import { client } from '../../../apis/axios';
+import { API_PATH } from '../../../constants/api';
+import useToast from '../../useToast';
+
+export const usePutReservationStatus = (
+  reservationId,
+  { onSuccess: handleSuccess } = { onSuccess: () => {} }
+) => {
+  const queryClient = useQueryClient();
+  const { insertToastItem } = useToast();
+
+  return useMutation(
+    async (status: string) => {
+      await client({
+        method: 'put',
+        url: `${API_PATH.RESERVATIONS}/${reservationId}`,
+        data: { status },
+      });
+    },
+    {
+      onSuccess: () => {
+        handleSuccess();
+        queryClient.invalidateQueries('reservations');
+        insertToastItem('✅ 요청에 성공했습니다.');
+      },
+      onError: () => {
+        insertToastItem('요청에 실패했습니다.');
+      },
+    }
+  );
+};
+>>>>>>> 277b11d (merge)
