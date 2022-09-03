5b8b64f7b4503b98763b1130abcf3fc7a7a90b9b
jayjaehunchoi <wogns0108@naver.com>
2022-09-03 16:05:23 +0900

    refactor: 스크립트 설명을 수정한다

--- backend/script/deploy.sh
+++ backend/script/deploy.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
+
 if [ $# -ne 2 ]; then
-        echo “please insert property environment like -e prod | --environment dev)”
+        echo "please insert property environment like ( -e prod | --environment dev )"
         exit 1
 fi
-if [ “$1" != “-e” ] && [ “$1" != “--environment” ]; then
-        echo “this argument not allowed : $1"
+
+if [ "$1" != "-e" ] && [ "$1" != "--environment" ]; then
+        echo "this argument not allowed : $1"
         exit 1
 fi
+
 ENVIRONMENT=$2
 PID=$(pgrep -f thankoo)
+
 function killProcess() {
-        echo “> 현재 프로세스 종료 작업 시작”
+        echo "> 현재 프로세스 종료 작업 시작"
         if [ -z $PID ]; then
-                echo “종료할 Process가 없습니다.”
+                echo "종료할 Process가 없습니다."
         else
                 kill -9 $PID
-                echo “$PID 프로세스 종료”
+                echo "$PID 프로세스 종료"
         fi
 }
+
 function startJar() {
-        echo “> 배포 시작”
-        nohup java -jar -Duser.timezone=“Asia/Seoul” -Dspring.profiles.active=$ENVIRONMENT thankoo-0.0.1-SNAPSHOT.jar --spring.config.location=classpath:/thankoo-backend-secret/application-$ENVIRONMENT.yml 1>thankoo.log 2>thankoo-error.log &
+        echo "> 배포 시작"
+        nohup java -jar -Duser.timezone="Asia/Seoul" -Dspring.profiles.active=$ENVIRONMENT thankoo-0.0.1-SNAPSHOT.jar --spring.config.location=classpath:/thankoo-backend-secret/application-$ENVIRONMENT.yml 1>thankoo.log 2>thankoo-error.log &
 }
+
 killProcess
 startJar
+
 exit 0
