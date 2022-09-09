4dbeb8d7337f875e6cd7e14e02d1a4fc2ce398a0
jayjaehunchoi <wogns0108@naver.com>
2022-09-09 14:08:00 +0900

    refactor: final 키워드 추가

--- backend/src/main/java/com/woowacourse/thankoo/common/domain/AlarmSpecification.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/domain/AlarmSpecification.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class AlarmSpecification {
 
-    public static String COUPON_SENT = "coupon_sent";
+    public static final String COUPON_SENT = "coupon_sent";
 
     private final String alarmType;
     private final List<Long> targetIds;
