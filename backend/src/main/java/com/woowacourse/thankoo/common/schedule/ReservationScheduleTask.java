26a0e1045b34a45d7f6b322ecbb6a4d709634aeb
sure-why-not <pkse1213@gmail.com>
2022-09-12 04:45:38 +0900

    feat: 스케줄링 로그 추가

--- backend/src/main/java/com/woowacourse/thankoo/common/schedule/ReservationScheduleTask.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/schedule/ReservationScheduleTask.java
@@ -3,6 +3,7 @@ package com.woowacourse.thankoo.common.schedule;
 import com.woowacourse.thankoo.reservation.application.ReservationService;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,14 @@ import org.springframework.stereotype.Component;
 @Profile("prod")
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ReservationScheduleTask {
 
     private final ReservationService reservationService;
 
     @Scheduled(cron = "0 0/30 10-19 * * *")
     void executeCancelExpiredReservation() {
+        log.debug("[Scheduling] 만료된 예약 취소 처리");
         reservationService.cancelExpiredReservation(LocalDateTime.now());
     }
 }
