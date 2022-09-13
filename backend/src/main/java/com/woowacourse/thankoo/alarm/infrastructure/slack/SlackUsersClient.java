fc100f069ca340d811ebcbf40d638b7e9bf5416c
jayjaehunchoi <wogns0108@naver.com>
2022-09-13 17:47:38 +0900

    feat: 슬랙 유저 메모리에 캐싱 (Look aside)

--- backend/src/main/java/com/woowacourse/thankoo/alarm/infrastructure/slack/SlackUsersClient.java
+++ backend/src/main/java/com/woowacourse/thankoo/alarm/infrastructure/slack/SlackUsersClient.java
@@ -1,15 +1,11 @@
-package com.woowacourse.thankoo.alarm.infrastructure;
+package com.woowacourse.thankoo.alarm.infrastructure.slack;
 
-import static org.springframework.http.HttpHeaders.*;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.woowacourse.thankoo.alarm.exception.InvalidAlarmException;
-import com.woowacourse.thankoo.alarm.infrastructure.dto.Attachments;
-import com.woowacourse.thankoo.alarm.infrastructure.dto.SlackMessageRequest;
 import com.woowacourse.thankoo.alarm.infrastructure.dto.SlackUserResponse;
 import com.woowacourse.thankoo.alarm.infrastructure.dto.SlackUsersResponse;
 import com.woowacourse.thankoo.common.exception.ErrorType;
@@ -20,46 +16,34 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class SlackClient {
+public class SlackUsersClient {
 
     private static final RestTemplate REST_TEMPLATE = new RestTemplate();
 
-    private static final String TOKEN_TYPE = "Bearer ";
-
-    private final String token;
+    private final String authorization;
     private final String usersUri;
-    private final String messageUri;
 
-    public SlackClient(@Value("${slack.token}") final String token,
-                       @Value("${slack.users-uri}") final String usersUri,
-                       @Value("${slack.message-uri}") final String messageUri) {
-        this.token = token;
+    public SlackUsersClient(@Value("${slack.authorization}") final String authorization,
+                            @Value("${slack.users-uri}") final String usersUri) {
+        this.authorization = authorization;
         this.usersUri = usersUri;
-        this.messageUri = messageUri;
     }
 
     public SlackUsersResponse getUsers() {
-        HttpEntity<SlackUsersResponse> request = new HttpEntity<>(setHeaders(APPLICATION_FORM_URLENCODED_VALUE));
+        HttpEntity<SlackUsersResponse> request = new HttpEntity<>(setHeaders());
         return REST_TEMPLATE
                 .exchange(usersUri, GET, request, SlackUsersResponse.class)
                 .getBody();
     }
 
-    public void sendMessage(final String channel, final Attachments attachments) {
-        SlackMessageRequest request = SlackMessageRequest.of(channel, attachments);
-        HttpHeaders headers = setHeaders(APPLICATION_JSON_VALUE);
-        HttpEntity<SlackMessageRequest> requestHttpEntity = new HttpEntity<>(request, headers);
-        REST_TEMPLATE.exchange(messageUri, POST, requestHttpEntity, Void.class);
-    }
-
-    private HttpHeaders setHeaders(final String contentType) {
+    private HttpHeaders setHeaders() {
         HttpHeaders headers = new HttpHeaders();
-        headers.add(AUTHORIZATION, TOKEN_TYPE + token);
-        headers.add(CONTENT_TYPE, contentType);
+        headers.add(AUTHORIZATION, authorization);
+        headers.add(CONTENT_TYPE, APPLICATION_JSON_VALUE);
         return headers;
     }
 
-    public String getUserToken(final String email) {
+    public String getUserTokenByEmail(final String email) {
         SlackUserResponse slackUser = getUser(email, getUsers());
         return slackUser.getUserToken();
     }
