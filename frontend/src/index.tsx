5def63e6669e3a47b3af5eb0c271dd97beb248be
Kang YunHo <72348351+KangYunHo1221@users.noreply.github.com>
2022-09-14 17:24:02 +0900

    feature: 웹팩 환경별 분리 (#387)
    * feat: dev-build 수정
    
    * feat: add webpack-merge 라이브러리
    
    * refactor: 불필요 console.log삭제
    
    * refactor: package script 수정
    
    * refactor: webpack 환경별로 파일 분리
    
    * refactor: google auth process.env별 분리
    
    * refactor: index.tsx의 process.env.MODE 이름 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,15 +8,16 @@ import { css, Global } from '@emotion/react';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
+import { queryClient } from './apis/queryClient';
 import './assets/favicon/favicon.ico';
 import reset from './styles/GlobalReset';
 import global from './styles/GlobalStyled';
 import { ThemeProvider } from './styles/ThemeProvider';
-import { queryClient } from './apis/queryClient';
 
 const rootElement = document.getElementById('root')!;
 const root = createRoot(rootElement);
-if (process.env.MODE === 'local') {
+
+if (process.env.MODE === 'noServer') {
   const { worker } = require('./mocks/browser');
   worker.start();
 }
