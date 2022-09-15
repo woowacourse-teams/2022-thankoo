2666d48e6a037e963736c58925a0cf73bb3dcf46
kwannee <41886825+kwannee@users.noreply.github.com>
2022-09-15 10:11:19 +0900

    feat: 폰트를 바꾼다. (#386)
    * feat: 첫 페이지 디자인 변경
    
    - 폰트 적용
    - html lang "ko"로 적용
    
    * feat: '땡쿠' 삭제
    
    - 라라가 빼라고 함
    
    * feat: index.html에서 구글 폰트 제거
    
    * refactor: 필요없는 파일 삭제

--- frontend/src/styles/GlobalStyled.tsx
+++ frontend/src/styles/GlobalStyled.tsx
@@ -2,7 +2,14 @@ import { css } from '@emotion/react';
 
 const global = css`
   * {
-    font-family: 'Noto Sans KR', sans-serif !important;
+    @font-face {
+      font-family: 'Pretendard-Regular';
+      src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+        format('woff');
+      font-weight: 400;
+      font-style: normal;
+    }
+    font-family: 'Pretendard-Regular' !important;
     -webkit-tap-highlight-color: transparent;
   }
   html {
