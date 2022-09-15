9ba5a514e05fdd58fdcb6e7be93aa54a1a46b1e7
sure-why-not <pkse1213@gmail.com>
2022-09-15 15:24:56 +0900

    feat: (admin) 쿠폰의 예약 상태를 변경하는 기능

---
+++ backend/src/main/java/com/woowacourse/thankoo/admin/reservation/domain/AdminReservationRepository.java
@@ -0,0 +1,16 @@
+package com.woowacourse.thankoo.admin.reservation.domain;
+
+import com.woowacourse.thankoo.coupon.domain.Coupon;
+import com.woowacourse.thankoo.reservation.domain.Reservation;
+import com.woowacourse.thankoo.reservation.domain.ReservationStatus;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface AdminReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Reservation r SET r.reservationStatus = :updatingStatus WHERE r.reservationStatus = :status AND r.coupon IN (:coupons)")
+    void updateReservationStatus(ReservationStatus status, ReservationStatus updatingStatus, List<Coupon> coupons);
+}
