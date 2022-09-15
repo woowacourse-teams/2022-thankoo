ff74c24f5677a2a83a60abb9008be09f5c925aa2
jayjaehunchoi <wogns0108@naver.com>
2022-09-15 11:30:42 +0900

    feat: 환경에 따라 다르게 알람에 전달되는 링크를 만드는 기능

---
+++ backend/src/main/java/com/woowacourse/thankoo/alarm/infrastructure/AlarmLinkGenerator.java
@@ -0,0 +1,25 @@
+package com.woowacourse.thankoo.alarm.infrastructure;
+
+import com.woowacourse.thankoo.alarm.exception.InvalidAlarmException;
+import com.woowacourse.thankoo.common.exception.ErrorType;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class AlarmLinkGenerator {
+
+    private final String rootUrl;
+
+    public AlarmLinkGenerator(@Value("${api.root}") final String rootUrl) {
+        this.rootUrl = rootUrl;
+    }
+
+    public String createUrl(final String url) {
+        if (!url.startsWith("/")) {
+            throw new InvalidAlarmException(ErrorType.INVALID_ALARM_LINK);
+        }
+        return rootUrl + url;
+    }
+}
