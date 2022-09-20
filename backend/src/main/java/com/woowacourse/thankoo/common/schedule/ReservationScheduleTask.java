747fa3c7d4ef1346294bfadd2e8069892c39c3e3
sure-why-not <pkse1213@gmail.com>
2022-09-20 16:23:00 +0900

    refactor: 만료된 예약 스케줄링시 처리하려 시간 설정 수정

--- backend/src/main/java/com/woowacourse/thankoo/common/schedule/ReservationScheduleTask.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/schedule/ReservationScheduleTask.java
@@ -2,6 +2,7 @@ package com.woowacourse.thankoo.common.schedule;
 
 import com.woowacourse.thankoo.reservation.application.ReservationService;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
@@ -19,6 +20,9 @@ public class ReservationScheduleTask {
     @Scheduled(cron = "0 0/30 10-19 * * *")
     void executeCancelExpiredReservation() {
         log.debug("[Scheduling] 만료된 예약 취소 처리");
-        reservationService.cancelExpiredReservation(LocalDateTime.now());
+        LocalDateTime nowDateTime = LocalDateTime.now();
+        LocalTime nowTime = LocalTime.of(nowDateTime.getHour(), nowDateTime.getMinute());
+        LocalDateTime reservationTime = LocalDateTime.of(nowDateTime.toLocalDate(), nowTime);
+        reservationService.cancelExpiredReservation(reservationTime);
     }
 }
