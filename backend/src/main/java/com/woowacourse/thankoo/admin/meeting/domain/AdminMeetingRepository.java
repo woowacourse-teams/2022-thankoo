3e283638ba1c731d8a7688e1fd9fbdb91e26f742
Jaehun Choi <wogns0108@naver.com>
2022-09-21 10:16:54 +0900

    refactor: 미팅 알람 메시지 리팩터링 (#427)
    * refactor: MeetingMessage NullPointerException 문제 해결
    
    * refactor: @Param 애노테이션 추가

--- backend/src/main/java/com/woowacourse/thankoo/admin/meeting/domain/AdminMeetingRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/admin/meeting/domain/AdminMeetingRepository.java
@@ -7,10 +7,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AdminMeetingRepository extends JpaRepository<Meeting, Long> {
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Meeting m SET m.meetingStatus = :updatingStatus WHERE m.meetingStatus = :status AND m.coupon IN (:coupons)")
-    void updateMeetingStatus(MeetingStatus status, MeetingStatus updatingStatus, List<Coupon> coupons);
+    void updateMeetingStatus(@Param("status") MeetingStatus status,
+                             @Param("updatingStatus") MeetingStatus updatingStatus,
+                             @Param("coupons") List<Coupon> coupons);
 }
--- backend/src/main/java/com/woowacourse/thankoo/admin/reservation/domain/AdminReservationRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/admin/reservation/domain/AdminReservationRepository.java
@@ -7,10 +7,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AdminReservationRepository extends JpaRepository<Reservation, Long> {
 
     @Modifying(clearAutomatically = true)
     @Query("UPDATE Reservation r SET r.reservationStatus = :updatingStatus WHERE r.reservationStatus = :status AND r.coupon IN (:coupons)")
-    void updateReservationStatus(ReservationStatus status, ReservationStatus updatingStatus, List<Coupon> coupons);
+    void updateReservationStatus(@Param("status") ReservationStatus status,
+                                 @Param("updatingStatus") ReservationStatus updatingStatus,
+                                 @Param("coupons") List<Coupon> coupons);
 }
