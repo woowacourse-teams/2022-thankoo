8e4cd3175ed07ef813e6acf69349fa3852b2a9a2
jayjaehunchoi <wogns0108@naver.com>
2022-09-29 15:29:04 +0900

    feat: Organization Name 생성

---
+++ backend/src/main/java/com/woowacourse/thankoo/organization/domain/OrganizationName.java
@@ -0,0 +1,57 @@
+package com.woowacourse.thankoo.organization.domain;
+
+import com.woowacourse.thankoo.common.exception.ErrorType;
+import com.woowacourse.thankoo.organization.exception.InvalidOrganizationException;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class OrganizationName {
+
+    private static final int NAME_MAX_LENGTH = 12;
+
+    @Column(name = "name", length = 50, unique = true)
+    private String value;
+
+    public OrganizationName(String value) {
+        value = value.strip();
+        validateName(value);
+        this.value = value;
+    }
+
+    private void validateName(final String name) {
+        if (name.isBlank() || name.length() > NAME_MAX_LENGTH) {
+            throw new InvalidOrganizationException(ErrorType.INVALID_ORGANIZATION_NAME);
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof OrganizationName)) {
+            return false;
+        }
+        OrganizationName that = (OrganizationName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "OrganizationName{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
---
+++ backend/src/test/java/com/woowacourse/thankoo/organization/domain/OrganizationNameTest.java
@@ -0,0 +1,45 @@
+package com.woowacourse.thankoo.organization.domain;
+
+import static com.woowacourse.thankoo.common.fixtures.OrganizationFixture.ORGANIZATION_NAME;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.woowacourse.thankoo.organization.exception.InvalidOrganizationException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("OrganizationName 은 ")
+class OrganizationNameTest {
+
+    @DisplayName("객체를 만들 때 ")
+    @Nested
+    class CreateTest {
+
+        @DisplayName("이름이 기준에 맞으면 성공한다.")
+        @Test
+        void success() {
+            assertDoesNotThrow(() -> new OrganizationName(ORGANIZATION_NAME));
+        }
+
+        @DisplayName("이름 기준에 맞지 않으면 실패한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {"", " ", "우아한테크코스의땡쿠BackendEngineer"})
+        void sizeFailed(final String value) {
+            assertThatThrownBy(() -> new OrganizationName(value))
+                    .isInstanceOf(InvalidOrganizationException.class)
+                    .hasMessage("잘못된 조직 이름입니다.");
+        }
+
+        @DisplayName("이름 양 끝의 공백을 제거한다.")
+        @ParameterizedTest
+        @ValueSource(strings = {" 우아한 테크 ", "우아한 테크 ", " 우아한 테크"})
+        void strip(final String value) {
+            OrganizationName organizationName = new OrganizationName(value);
+            assertThat(organizationName.getValue()).isEqualTo("우아한 테크");
+        }
+    }
+}
