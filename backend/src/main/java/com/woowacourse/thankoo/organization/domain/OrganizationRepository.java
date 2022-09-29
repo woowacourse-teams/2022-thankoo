2ea83e6a0dd93ca12e250e08e3401401b984ba3b
jayjaehunchoi <wogns0108@naver.com>
2022-09-29 17:35:51 +0900

    feat: Organization code, 이름 중복체크 추가

--- backend/src/main/java/com/woowacourse/thankoo/organization/domain/OrganizationRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/organization/domain/OrganizationRepository.java
@@ -3,4 +3,8 @@ package com.woowacourse.thankoo.organization.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+
+    boolean existsByCode(OrganizationCode code);
+
+    boolean existsByName(OrganizationName name);
 }
