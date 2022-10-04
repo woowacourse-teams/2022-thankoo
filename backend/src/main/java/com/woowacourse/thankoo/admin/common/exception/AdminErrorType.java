029c2949973458f6952d34509a48c85ecd2c9422
skullKim <kyk990328@gmail.com>
2022-10-04 20:10:40 +0900

    refactor: 에러 메시지 변경

--- backend/src/main/java/com/woowacourse/thankoo/admin/common/exception/AdminErrorType.java
+++ backend/src/main/java/com/woowacourse/thankoo/admin/common/exception/AdminErrorType.java
@@ -16,9 +16,9 @@ public enum AdminErrorType {
     DUPLICATE_COUPON_SERIAL("시리얼 번호가 중복됩니다."),
     INVALID_COUPON_SERIAL_SIZE("생성할 수 있는 시리얼 번호를 초과했습니다."),
 
-    CANNOT_ENCRYPT_PASSWORD("비밀번호 암호화 중 에러가 발생했습니다."),
+    CANNOT_ENCRYPT_PASSWORD("비밀번호를 암호화할 수 없습니다."),
     INVALID_LOGIN_INFORMATION("올바르지 않은 비밀번호 또는 이름입니다."),
-    NOT_AUTHENTICATED("인증 되지 않은 어드민입니다."),
+    NOT_AUTHENTICATED("인증되지 않은 어드민입니다."),
     ALREADY_AUTHENTICATED("이미 인증된 어드민입니다."),
     INVALID_TOKEN("유효하지 않은 토큰입니다");
 
