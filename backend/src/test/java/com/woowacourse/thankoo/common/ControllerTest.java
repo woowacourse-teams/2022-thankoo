4d87afb5981c1b5683f98d6ae30e3cbf56906487
skullKim <kyk990328@gmail.com>
2022-10-04 20:28:44 +0900

    refactor: 코딩 컨벤션에 맞지 않는 코드 수정

--- backend/src/test/java/com/woowacourse/thankoo/common/ControllerTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/common/ControllerTest.java
@@ -60,6 +60,9 @@ public class ControllerTest {
     protected AuthenticationService authenticationService;
 
     @MockBean
+    protected TokenDecoder tokenDecoder;
+
+    @MockBean
     protected JwtTokenProvider jwtTokenProvider;
 
     @MockBean
