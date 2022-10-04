255a6dd33be9fcd29afb29e89b904d6f23c08b86
skullKim <kyk990328@gmail.com>
2022-10-04 20:01:46 +0900

    refactor: 테스트 방식 변경

--- backend/src/test/java/com/woowacourse/thankoo/admin/authentication/domain/PasswordEncryptionTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/admin/authentication/domain/PasswordEncryptionTest.java
@@ -2,6 +2,7 @@ package com.woowacourse.thankoo.admin.authentication.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.woowacourse.thankoo.admin.authentication.infrastructure.PasswordHashEncryption;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @DisplayName("PasswordEncryption 은 ")
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class PasswordEncryptionTest {
-
-    @Autowired
-    private PasswordEncryption passwordEncryption;
 
     @DisplayName("주어진 비밀번호를 암호화해야 한다.")
     @Test
     void encrypt() {
+        PasswordEncryption passwordEncryption = new PasswordHashEncryption("csclxnexia70000000", 65536, 128);
         String encryptedPassword = passwordEncryption.encrypt("password");
 
         assertThat(encryptedPassword).isNotEqualTo("password");
