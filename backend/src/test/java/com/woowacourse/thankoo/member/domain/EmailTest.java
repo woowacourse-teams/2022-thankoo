99dc5077fac97be8e838cadc22e5947ef8242add
yhh1056 <yhh1056@naver.com>
2022-10-06 00:52:02 +0900

    test: member vo 객체에 대한 테스트 추가

--- backend/src/test/java/com/woowacourse/thankoo/member/domain/EmailTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/member/domain/EmailTest.java
@@ -1,10 +1,12 @@
 package com.woowacourse.thankoo.member.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.woowacourse.thankoo.member.exception.InvalidMemberException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,7 +14,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 class EmailTest {
 
     @DisplayName("올바르지 않은 이메일 값으로 생성하면 예외가 발생한다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "email : {0}")
     @ValueSource(strings = {" ", "abcdefghijkabcdefghijk1", "abc@abc"})
     void createWithInvalidEmailException(final String value) {
         assertThatThrownBy(() -> new Email(value))
@@ -21,10 +23,18 @@ class EmailTest {
     }
 
     @DisplayName("올바른 이메일은 성공한다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "email : {0}")
     @ValueSource(strings = {"huni@huni.com", "huni@huni.co.kr", "huni_thankoo@huni.com", "huni.a@huni.com",
             "hu.ni.dev@huni.com"})
     void create(final String value) {
         assertDoesNotThrow(() -> new Email(value));
+    }
+
+    @DisplayName("동일한 이메일인지 확인한다. ")
+    @Test
+    void equals() {
+        Email email = new Email("huni@huni.com");
+
+        assertThat(email).isEqualTo(new Email("huni@huni.com"));
     }
 }
--- backend/src/test/java/com/woowacourse/thankoo/member/domain/NameTest.java
+++ backend/src/test/java/com/woowacourse/thankoo/member/domain/NameTest.java
@@ -14,17 +14,26 @@ class NameTest {
 
     @DisplayName("이름의 앞/뒤에 공백이 있을 경우 제거한다.")
     @Test
-    void CouponContentMessageTrim() {
+    void couponContentMessageTrim() {
         Name name = new Name(" name ");
+
         assertThat(name.getValue()).isEqualTo("name");
     }
 
-    @DisplayName("올바르지 않은 이름이 들어올 경우 예외가 발생한다.")
-    @ParameterizedTest
+    @DisplayName("이름에 공백이거나 유효하지 않은 길이인 경우 예외가 발생한다.")
+    @ParameterizedTest(name = "name : {0}")
     @ValueSource(strings = {" ", "abcdef"})
     void createNameException(String value) {
         assertThatThrownBy(() -> new Name(value))
                 .isInstanceOf(InvalidMemberException.class)
                 .hasMessage("올바르지 않은 이름입니다.");
+    }
+
+    @DisplayName("이름이 동일한지 확인한다.")
+    @Test
+    void equals() {
+        Name name = new Name("hoho");
+
+        assertThat(name).isEqualTo(new Name("hoho"));
     }
 }
