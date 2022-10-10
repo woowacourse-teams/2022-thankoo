b4e05752f3eb9e33e1eb676d89d76e7058471b41
yhh1056 <yhh1056@naver.com>
2022-10-10 16:07:35 +0900

    refactor: adminRepository를 의존하도록 수정

--- backend/src/main/java/com/woowacourse/thankoo/admin/serial/application/AdminCouponSerialService.java
+++ backend/src/main/java/com/woowacourse/thankoo/admin/serial/application/AdminCouponSerialService.java
@@ -1,19 +1,19 @@
 package com.woowacourse.thankoo.admin.serial.application;
 
 import com.woowacourse.thankoo.admin.common.exception.AdminErrorType;
+import com.woowacourse.thankoo.admin.member.domain.AdminMemberRepository;
 import com.woowacourse.thankoo.admin.member.exception.AdminNotFoundMemberException;
 import com.woowacourse.thankoo.admin.organization.domain.AdminOrganizationRepository;
 import com.woowacourse.thankoo.admin.organization.exception.AdminInvalidOrganizationException;
 import com.woowacourse.thankoo.admin.serial.application.dto.AdminCouponSerialRequest;
 import com.woowacourse.thankoo.admin.serial.domain.AdminCodeCreator;
 import com.woowacourse.thankoo.admin.serial.domain.AdminCouponSerialQueryRepository;
+import com.woowacourse.thankoo.admin.serial.domain.AdminCouponSerialRepository;
 import com.woowacourse.thankoo.admin.serial.domain.AdminSerialCodes;
 import com.woowacourse.thankoo.admin.serial.excepion.AdminInvalidCouponSerialException;
 import com.woowacourse.thankoo.member.domain.Member;
-import com.woowacourse.thankoo.member.domain.MemberRepository;
 import com.woowacourse.thankoo.organization.domain.Organization;
 import com.woowacourse.thankoo.serial.domain.CouponSerial;
-import com.woowacourse.thankoo.serial.domain.CouponSerialRepository;
 import com.woowacourse.thankoo.serial.domain.SerialCode;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,8 +27,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AdminCouponSerialService {
 
     private final AdminCouponSerialQueryRepository couponSerialQueryRepository;
-    private final CouponSerialRepository couponSerialRepository;
-    private final MemberRepository memberRepository;
+    private final AdminCouponSerialRepository couponSerialRepository;
+    private final AdminMemberRepository memberRepository;
     private final AdminOrganizationRepository organizationRepository;
     private final AdminCodeCreator adminCodeCreator;
 
