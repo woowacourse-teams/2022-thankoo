bb8085e963dd4141791a5fe6d26874dab572d76b
kwannee <yleh1126@naver.com>
2022-10-12 01:13:23 +0900

    feat: Dropdown 컴포넌트 추가 및 organization 변경 dropdown 구현

---
+++ frontend/src/components/@shared/Dropdown.tsx
@@ -0,0 +1,124 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { createContext, Dispatch, SetStateAction, useContext, useState } from 'react';
+import Dimmer from './Dimmer';
+
+type ToggleContextState = {
+  toggle: boolean;
+  setToggle: Dispatch<SetStateAction<boolean>>;
+};
+
+const ToggleContext = createContext<ToggleContextState>({
+  toggle: false,
+  setToggle: () => {},
+});
+
+const Dropdown = ({ children, ...props }) => {
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  return (
+    <ToggleContext.Provider value={{ toggle, setToggle }}>
+      <Container {...props}>{children}</Container>
+    </ToggleContext.Provider>
+  );
+};
+
+Dropdown.Toggle = ({ children, ...props }) => {
+  const { setToggle } = useContext(ToggleContext);
+
+  return (
+    <Toggle
+      onClick={() => {
+        setToggle(prev => !prev);
+      }}
+      {...props}
+    >
+      {children}
+    </Toggle>
+  );
+};
+
+Dropdown.Menu = ({ children, ...props }) => {
+  const { setToggle, toggle } = useContext(ToggleContext);
+
+  return (
+    <>
+      <Menu show={toggle} {...props}>
+        {children}
+      </Menu>
+      <Dimmer
+        onClick={() => {
+          setToggle(false);
+        }}
+        show={toggle}
+      />
+    </>
+  );
+};
+
+Dropdown.Item = ({ children, selected, ...props }) => {
+  return (
+    <Item selected={selected} {...props}>
+      {children}
+    </Item>
+  );
+};
+
+type MenuProps = {
+  show: boolean;
+};
+
+type ItemProps = {
+  selected: boolean;
+};
+
+const Container = styled.section`
+  position: relative;
+`;
+const Menu = styled.ul<MenuProps>`
+  position: absolute;
+  top: 170%;
+  left: 0;
+
+  display: flex;
+  flex-flow: column;
+
+  width: max-content;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 7px;
+
+  z-index: 1000;
+  cursor: pointer;
+  ${({ show }) =>
+    show
+      ? css`
+          display: flex;
+        `
+      : css`
+          display: none;
+        `}
+`;
+const Toggle = styled.span`
+  display: flex;
+  align-items: center;
+  color: white;
+  font-size: 1.7rem;
+  letter-spacing: 0.1rem;
+  cursor: pointer;
+`;
+const Item = styled.li<ItemProps>`
+  ${({ selected }) =>
+    selected
+      ? css`
+          color: black;
+          font-weight: bold;
+        `
+      : css`
+          color: #656f7a;
+        `}
+  font-size: 1.3rem;
+  padding: 1.4rem 1rem;
+`;
+
+export default Dropdown;
--- frontend/src/layout/HeaderAndNavLayout.tsx
+++ frontend/src/layout/HeaderAndNavLayout.tsx
@@ -1,13 +1,53 @@
 import styled from '@emotion/styled';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import BottomNavBar from '../components/PageButton/BottomNavBar';
 import UserProfileButton from '../components/@shared/UserProfileButton';
 import PageLayout from './PageLayout';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import Dropdown from '../components/@shared/Dropdown';
+
+const ORGANIZATIONS = [
+  {
+    organizationId: 1,
+    organizationName: '땡쿠',
+    organizationCode: 'THANKOO1',
+    orderNumber: 1,
+    lastAccessed: true,
+  },
+  {
+    organizationId: 2,
+    organizationName: '우아한테크코스 4기',
+    organizationCode: 'WOOWACO1',
+    orderNumber: 2,
+    lastAccessed: false,
+  },
+];
 
 export default () => {
+  // const { data: selectedOrganization } = useGetOrganizations();
+  const selectedOrganization = ORGANIZATIONS[0];
+
   return (
     <PageLayout>
       <UserProfileWrapper>
+        <Dropdown>
+          <Dropdown.Toggle>
+            {selectedOrganization.organizationName}
+            <ArrowDropDownIcon />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {ORGANIZATIONS.map(organization => (
+              <Link to={`/${organization.organizationCode}`} reloadDocument={true}>
+                <Dropdown.Item
+                  key={organization.organizationId}
+                  selected={organization.organizationName === selectedOrganization.organizationName}
+                >
+                  {organization.organizationName}
+                </Dropdown.Item>
+              </Link>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
         <UserProfileButton />
       </UserProfileWrapper>
       <Outlet />
@@ -18,6 +58,7 @@ export default () => {
 
 const UserProfileWrapper = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
   margin: 1rem;
 `;
