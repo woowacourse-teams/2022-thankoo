cdb89a991cfa60cc51eeb0108c91bca218bfd4a2
skullKim <kyk990328@gmail.com>
2022-10-13 11:02:19 +0900

    style: 코딩 컨벤션에 맞추어 코드 변경

--- backend/src/main/java/com/woowacourse/thankoo/admin/organization/application/dto/AdminOrganizationCreationRequest.java
+++ backend/src/main/java/com/woowacourse/thankoo/admin/organization/application/dto/AdminOrganizationCreationRequest.java
@@ -4,8 +4,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class AdminOrganizationCreationRequest {
 
     private String name;
