5db3c2abd4456c60a8c356d1bb0dcf607106641f
yhh1056 <yhh1056@naver.com>
2022-10-14 15:45:28 +0900

    chore: local 작업을 위한 임시 브랜치 커밋

--- backend/src/main/java/com/woowacourse/thankoo/ThankooApplication.java
+++ backend/src/main/java/com/woowacourse/thankoo/ThankooApplication.java
@@ -1,7 +1,15 @@
 package com.woowacourse.thankoo;
 
+import com.woowacourse.thankoo.organization.domain.CodeGenerator;
+import com.woowacourse.thankoo.organization.domain.Organization;
+import com.woowacourse.thankoo.organization.domain.OrganizationRepository;
+import com.woowacourse.thankoo.organization.domain.OrganizationValidator;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ThankooApplication {
@@ -10,4 +18,23 @@ public class ThankooApplication {
         SpringApplication.run(ThankooApplication.class, args);
     }
 
+    @Component
+    @RequiredArgsConstructor
+    static class Init {
+
+        private final OrganizationRepository organizationRepository;
+        private final OrganizationValidator organizationValidator;
+
+        @PostConstruct
+        public void set() {
+            CodeGenerator codeGenerator1 = length -> "00000001";
+            CodeGenerator codeGenerator2 = length -> "00000002";
+            CodeGenerator codeGenerator3 = length -> "00000003";
+
+            organizationRepository.saveAll(
+                    List.of(Organization.create("양은희파", codeGenerator1, 500, organizationValidator),
+                            Organization.create("삼거리파", codeGenerator2, 200, organizationValidator),
+                            Organization.create("라라라파", codeGenerator3, 100, organizationValidator)));
+        }
+    }
 }
