bb6a763ab781045174cda8f34ace32e74270d144
Yunki Kim <37966705+skullkim@users.noreply.github.com>
2022-10-14 11:15:11 +0900

    feat: 어드민 API 예외를 핸들링한다. (#548)
    * feat: 어드민 API 예외 핸들링
    
    * style: 코딩 컨벤션에 맞추어 코드 수정
    
    * refactor: 오타 수정

---
+++ backend/src/main/java/com/woowacourse/thankoo/admin/common/exception/AdminControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.woowacourse.thankoo.admin.common.exception;
+
+import com.woowacourse.thankoo.admin.common.exception.dto.AdminErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class AdminControllerAdvice {
+
+    @ExceptionHandler(AdminBadRequestException.class)
+    public ResponseEntity<AdminErrorResponse> adminBadRequestExceptionHandler(final AdminBadRequestException e) {
+        log.warn("Admin Bad Request Exception", e);
+        return ResponseEntity.badRequest().body(new AdminErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(AdminUnAuthenticationException.class)
+    public ResponseEntity<AdminErrorResponse> adminUnAuthenticationExceptionHandler(final AdminUnAuthenticationException e) {
+        log.warn("Admin UnAuthentication Exception", e);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new AdminErrorResponse(e.getMessage()));
+    }
+}
