2ceed4b7e3a5e414d7155e0dd4e8953b1139202d
jayjaehunchoi <wogns0108@naver.com>
2022-10-18 07:15:43 +0900

    refactor: 불필요한 코드 제거

--- backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
-    Optional<Heart> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
-
     Optional<Heart> findBySenderIdAndReceiverIdAndOrganizationId(Long senderId, Long receiverId, Long organizationId);
 
     List<Heart> findByOrganizationIdAndSenderIdAndLast(Long organizationId, Long senderId, boolean last);
