a3d5dad1ba48c8ac210e459ed336bb1dbf521294
kwannee <yleh1126@naver.com>
2022-10-17 16:25:03 +0900

    refactor: 역할 분리

--- frontend/src/routes/AccessController.tsx
+++ frontend/src/routes/AccessController.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { interceptRequest } from '../apis/axios';
 import Spinner from '../components/@shared/Spinner';
@@ -5,19 +6,23 @@ import { ROUTE_PATH } from '../constants/routes';
 import { useGetOrganizations } from '../hooks/@queries/organization';
 
 export const AccessController = () => {
-  const { data: organizations } = useGetOrganizations();
+  const { data: organizations, isLoading } = useGetOrganizations();
 
   const organization = organizations?.find(organization => organization.lastAccessed);
 
-  if (typeof organizations === 'undefined') {
+  if (typeof organizations === 'undefined' || isLoading) {
     return <Spinner />;
   }
 
-  interceptRequest(String(organization?.organizationId));
-
   return organizations?.length ? (
-    <Outlet />
+    <OrganizationInjector organization={organization} />
   ) : (
     <Navigate to={ROUTE_PATH.JOIN_ORGANIZATION} replace />
   );
+};
+
+const OrganizationInjector = ({ organization }) => {
+  interceptRequest(String(organization.organizationId));
+
+  return <Outlet />;
 };
