e07600afa33c6dc570b18c2531b7e5e6606dbf56
jayjaehunchoi <wogns0108@naver.com>
2022-10-18 07:15:23 +0900

    refactor: 조직이 다르다면 같은 회원에게도 마음을 보낼 수 있도록 수정

--- backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/heart/domain/HeartRepository.java
@@ -8,6 +8,8 @@ public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     Optional<Heart> findBySenderIdAndReceiverId(Long senderId, Long receiverId);
 
+    Optional<Heart> findBySenderIdAndReceiverIdAndOrganizationId(Long senderId, Long receiverId, Long organizationId);
+
     List<Heart> findByOrganizationIdAndSenderIdAndLast(Long organizationId, Long senderId, boolean last);
 
     List<Heart> findByOrganizationIdAndReceiverIdAndLast(Long organizationId, Long receiverId, boolean last);
