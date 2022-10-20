95d0ffaaa7e6c99c2b84cd70445c204afadccbc1
jayjaehunchoi <wogns0108@naver.com>
2022-10-20 22:24:47 +0900

    refactor: findByCode 쿼리 수정

--- backend/src/main/java/com/woowacourse/thankoo/serial/domain/CouponSerialQueryRepository.java
+++ backend/src/main/java/com/woowacourse/thankoo/serial/domain/CouponSerialQueryRepository.java
@@ -30,11 +30,9 @@ public class CouponSerialQueryRepository {
 
     public Optional<CouponSerialMember> findByCode(final String code) {
         String sql =
-                "SELECT s.id, o.id AS organization_id, s.code, m.id AS sender_id, m.name AS sender_name, s.coupon_type, s.status "
+                "SELECT s.id, s.organization_id, s.code, m.id AS sender_id, m.name AS sender_name, s.coupon_type, s.status "
                         + "FROM coupon_serial AS s "
-                        + "JOIN member AS m ON s.sender_id = m.id "
-                        + "JOIN organization_member AS am ON m.id = am.id "
-                        + "JOIN organization AS o ON am.organization_id = o.id "
+                        + "LEFT JOIN member AS m ON s.sender_id = m.id "
                         + "WHERE s.code = :code";
 
         MapSqlParameterSource sqlParameterSource = new MapSqlParameterSource()
