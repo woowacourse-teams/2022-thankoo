9386dc8bbbceb7998d0c5c4a627c0fa4b6553f52
yhh1056 <yhh1056@naver.com>
2022-10-20 20:03:42 +0900

    feat: prod 환경 추가

--- backend/src/main/java/com/woowacourse/thankoo/common/config/ReplicationDataSourceConfig.java
+++ backend/src/main/java/com/woowacourse/thankoo/common/config/ReplicationDataSourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("dev")
+@Profile({"prod", "dev"})
 @Configuration
 public class ReplicationDataSourceConfig {
 
