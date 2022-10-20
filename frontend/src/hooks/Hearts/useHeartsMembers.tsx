54712e30129e44e3f29c18aec6c0b6ba4b700d34
Kang YunHo <72348351+KangYunHo1221@users.noreply.github.com>
2022-10-20 18:21:27 +0900

    feat: 마음보내기 페이지의 정렬 순서를 변경한다 (#597)

--- frontend/src/hooks/Hearts/useHeartsMembers.tsx
+++ frontend/src/hooks/Hearts/useHeartsMembers.tsx
@@ -8,11 +8,35 @@ const useHeartsMembers = (searchKeyword: string) => {
   const { data: heartHistory } = useGetHearts(); //
   const { mutate: postHeart } = usePostHeartMutation();
 
-  const matchedUsers = useFilterMatchedUser(searchKeyword, members);
+  const searchedUsers = useFilterMatchedUser(searchKeyword, members);
   const sentMembers = heartHistory?.sent!;
   const receivedMembers = heartHistory?.received!;
 
-  return { matchedUsers, sentMembers, receivedMembers, postHeart };
+  const searchedUserWithState = searchedUsers?.map(user => {
+    const canSend =
+      !sentMembers?.some(sentHistory => sentHistory.receiverId === user.id) ||
+      receivedMembers?.some(receivedHistory => receivedHistory.senderId === user.id);
+
+    const count = sentMembers?.find(sentHistory => sentHistory.receiverId === user.id)?.count;
+    const lastReceived =
+      sentMembers?.find(sentHistory => sentHistory.receiverId === user.id)?.modifiedAt ||
+      receivedMembers?.find(receiveHistory => receiveHistory.senderId === user.id)?.modifiedAt;
+    const modifiedLastReceived = lastReceived?.split(' ')[0];
+    const receivedUserCount = receivedMembers?.find(
+      receiveHistory => receiveHistory.senderId === user.id
+    )?.count;
+    return {
+      user: user,
+      canSend: canSend,
+      count: count,
+      modifiedLastReceived: modifiedLastReceived,
+      receivedUserCount: receivedUserCount,
+    };
+  });
+
+  searchedUserWithState.sort((a, b) => b?.count - a?.count);
+
+  return { searchedUserWithState, postHeart };
 };
 
 export default useHeartsMembers;
