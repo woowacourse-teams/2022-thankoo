dafacc3cf88b5f80dc98f499b33c6399e4aca225
Yunho <ansghkdbsgh@gmail.com>
2022-10-27 11:44:08 +0900

    refactor: ListViewMeetings 타입추가 및 네이밍 변경

--- frontend/src/components/@shared/ConditionalViewer.tsx
+++ frontend/src/components/@shared/ConditionalViewer.tsx
@@ -1,5 +1,13 @@
-const ConditionalViewer = ({ children, condition, replacement }) => {
-  return <>{condition ? children : replacement}</>;
+import { PropsWithChildren } from 'react';
+
+type ConditionalViewerProps = { showReplacement: boolean; replacement: React.ReactNode };
+
+const ConditionalViewer = ({
+  children,
+  showReplacement: condition,
+  replacement,
+}: PropsWithChildren<ConditionalViewerProps>) => {
+  return <>{condition ? replacement : children}</>;
 };
 
 export default ConditionalViewer;
