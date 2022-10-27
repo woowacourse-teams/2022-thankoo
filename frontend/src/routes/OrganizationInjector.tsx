f14929279e66b3432ef94a5228edc2387115e3e1
kwannee <yleh1126@naver.com>
2022-10-27 20:15:54 +0900

    refactor: OrganizationInjector Suspense 제거

--- frontend/src/routes/OrganizationInjector.tsx
+++ frontend/src/routes/OrganizationInjector.tsx
@@ -1,26 +1,18 @@
-import { Suspense } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { injectOrganizationToRequest } from '../apis/axios';
-import Spinner from '../components/@shared/Spinner';
 import { ROUTE_PATH } from '../constants/routes';
 import { useGetLastAccessedOrganization } from '../hooks/@queries/organization';
 
 const OrganizationInjector = () => {
-  return (
-    <Suspense fallback={<Spinner />}>
-      {(() => {
-        const lastAccessedOrganization = useGetLastAccessedOrganization();
+  const lastAccessedOrganization = useGetLastAccessedOrganization();
 
-        if (lastAccessedOrganization.organizationId) {
-          injectOrganizationToRequest(String(lastAccessedOrganization.organizationId));
+  if (!lastAccessedOrganization) {
+    return <Navigate to={ROUTE_PATH.JOIN_ORGANIZATION} />;
+  }
 
-          return <Outlet />;
-        }
+  injectOrganizationToRequest(String(lastAccessedOrganization.organizationId));
 
-        return <Navigate to={ROUTE_PATH.JOIN_ORGANIZATION} />;
-      })()}
-    </Suspense>
-  );
+  return <Outlet />;
 };
 
 export default OrganizationInjector;
