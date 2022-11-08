bd43313c33385ab79ad901ab3dad57448aa71528
kwannee <yleh1126@naver.com>
2022-11-08 14:55:31 +0900

    refactor: UserProfileButton => Avatar 사용으로 변경

--- frontend/src/components/@shared/UserProfileButton.tsx
+++ frontend/src/components/@shared/UserProfileButton.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 import { ROUTE_PATH } from '../../constants/routes';
 import { useGetUserProfile } from '../../hooks/@queries/profile';
-import ProfileIcon from './ProfileIcon';
+import Avatar from '../Avatar';
 
 const UserProfileButton = () => {
   const { data: profile } = useGetUserProfile();
 
   return (
     <Link to={ROUTE_PATH.PROFILE}>
-      <ProfileIcon src={profile ? profile?.imageUrl : 'default'} size={'28px'} />
+      <Avatar src={profile?.imageUrl} userName={profile?.name} />
     </Link>
   );
 };
